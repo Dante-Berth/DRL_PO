1843311868a049335cf31fd2019ec4a618525309
Dante-Berth <bertinalexAI@protonmail.com>
2025-11-24 20:36:32 +0100

    [UPDATE] utils replay buffer, nn name, algo (rl base env)

--- src/utils/nn.py
+++ src/utils/nn.py
@@ -10,8 +10,8 @@ import numpy as np
 class SoftQNetwork(nn.Module):
     def __init__(self, env):
         super().__init__()
-        obs_dim = int(np.array(env.single_observation_space.shape).prod())
-        act_dim = int(np.prod(env.single_action_space.shape))
+        obs_dim = int(np.array(env.observation_space.shape).prod())
+        act_dim = int(np.prod(env.action_space.shape))
         self.fc1 = nn.Linear(obs_dim + act_dim, 256)
         self.fc2 = nn.Linear(256, 256)
         self.fc3 = nn.Linear(256, 1)
@@ -24,31 +24,29 @@ class SoftQNetwork(nn.Module):
         return x
 
 
-LOG_STD_MAX = 2
-LOG_STD_MIN = -5
-
-
 class ActorContinous(nn.Module):
-    def __init__(self, env):
+    def __init__(self, env, log_std_max=2, log_std_min=-5):
         super().__init__()
-        obs_dim = int(np.array(env.single_observation_space.shape).prod())
-        act_dim = int(np.prod(env.single_action_space.shape))
+        obs_dim = int(np.array(env.observation_space.shape).prod())
+        act_dim = int(np.prod(env.action_space.shape))
         self.fc1 = nn.Linear(obs_dim, 256)
         self.fc2 = nn.Linear(256, 256)
         self.fc_mean = nn.Linear(256, act_dim)
         self.fc_logstd = nn.Linear(256, act_dim)
+        self.log_std_max = log_std_max
+        self.log_std_min = log_std_min
         # action rescaling
         self.register_buffer(
             "action_scale",
             torch.tensor(
-                (env.single_action_space.high - env.single_action_space.low) / 2.0,
+                (env.action_space.high - env.action_space.low) / 2.0,
                 dtype=torch.float32,
             ),
         )
         self.register_buffer(
             "action_bias",
             torch.tensor(
-                (env.single_action_space.high + env.single_action_space.low) / 2.0,
+                (env.action_space.high + env.action_space.low) / 2.0,
                 dtype=torch.float32,
             ),
         )
@@ -59,7 +57,9 @@ class ActorContinous(nn.Module):
         mean = self.fc_mean(x)
         log_std = self.fc_logstd(x)
         log_std = torch.tanh(log_std)
-        log_std = LOG_STD_MIN + 0.5 * (LOG_STD_MAX - LOG_STD_MIN) * (log_std + 1)
+        log_std = self.log_std_min + 0.5 * (self.log_std_max - self.log_std_min) * (
+            log_std + 1
+        )
         return mean, log_std
 
     def get_action(self, x):
@@ -75,3 +75,14 @@ class ActorContinous(nn.Module):
         log_prob = log_prob.sum(1, keepdim=True)
         mean_action = torch.tanh(mean) * self.action_scale + self.action_bias
         return action, log_prob, mean_action
+
+
+if __name__ == "__main__":
+    import src.envs
+    import gymnasium as gym
+
+    env = gym.make("OUTradingEnv-v0", psi=2.5)
+    obs, info = env.reset()
+    actor = ActorContinous(env)
+    critic = SoftQNetwork(env)
+    print("Still working src/utils/nn.py")
